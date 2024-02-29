1d524086a09b385393552042c440cb3a31fb9cd6
Abhishek <abhidograaa19@gmail.com>
2024-02-29 14:23:08 +0530

    Resolved issues in store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,30 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
 import mindGuideReducer from "./mindGuideSlice";
-const persistConfig = {
-  key: "root",
-  version: 1,
-  storage,
-};
-const persistedReducer = persistReducer(persistConfig, mindGuideReducer);
 export const store = configureStore({
-  reducer: { mindGuide: persistedReducer },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    mindGuide: mindGuideReducer,
+  },
 });
-
-export let persistor = persistStore(store);
