39d138113b35a418fe47fd912ab332d5750f01fd
Abhishek <abhidograaa19@gmail.com>
2024-03-05 15:43:42 +0530

    Added Report Download feature.

--- client/src/components/ReportModal.jsx
+++ client/src/components/ReportModal.jsx
@@ -1,14 +1,58 @@
-import React, { useState } from "react";
+import { useState, useRef } from "react";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
 import { useSelector } from "react-redux";
 import ReactMarkdown from "react-markdown";
+import html2pdf from "html2pdf.js";
+
 const ReportModal = ({ report }) => {
   const [open, setOpen] = useState(false);
+  const contentRef = useRef(null);
+  const userInfo = useSelector((state) => state.mindGuide.userInfo);
 
   const onOpenModal = () => setOpen(true);
   const onCloseModal = () => setOpen(false);
-  const userInfo = useSelector((state) => state.mindGuide.userInfo);
+
+  // const downloadReport = () => {
+  //   const content = contentRef.current;
+
+  //   if (content) {
+  //     const pdfOptions = {
+  //       margin: 10,
+  //       filename: "report.pdf",
+  //       image: { type: "jpeg", quality: 0.98 },
+  //       html2canvas: { scale: 2 },
+  //       jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+  //     };
+
+  //     html2pdf().from(content).set(pdfOptions).save();
+  //   }
+  // };
+
+  const downloadReport = () => {
+    const content = contentRef.current;
+
+    if (content) {
+      // Clone the content to create a separate HTML element for PDF conversion
+      const pdfContent = content.cloneNode(true);
+
+      // Remove the button from the PDF content
+      const buttonToRemove = pdfContent.querySelector("button");
+      if (buttonToRemove) {
+        buttonToRemove.remove();
+      }
+
+      const pdfOptions = {
+        margin: 10,
+        filename: "report.pdf",
+        image: { type: "jpeg", quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+      };
+
+      html2pdf().from(pdfContent).set(pdfOptions).save();
+    }
+  };
 
   return (
     <div>
@@ -19,7 +63,7 @@ const ReportModal = ({ report }) => {
         center
         classNames={{ modal: "custom-modal" }}
       >
-        <div className="modal-content">
+        <div className="modal-content" ref={contentRef}>
           <h2>
             Greetings{" "}
             <span className="capitalize underline font-extrabold">
@@ -28,9 +72,16 @@ const ReportModal = ({ report }) => {
             , Here is your <span className="text-green-600">report</span>.
           </h2>
           <ReactMarkdown>{report}</ReactMarkdown>
-          <button className="px-4 py-2 text-white bg-green-700 rounded-lg">
-            Download Report
-          </button>
+          <div style={{ marginTop: "10px" }}>
+            <button
+              onClick={() => {
+                downloadReport();
+              }}
+              className="px-4 py-2  text-white bg-green-700 rounded-lg"
+            >
+              Download Report
+            </button>
+          </div>
         </div>
       </Modal>
     </div>
