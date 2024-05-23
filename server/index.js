9e6a34c3aefd57b2582bf71dfeb1c62f6178e816
Abhishek <abhidograaa19@gmail.com>
2024-05-23 12:05:49 +0530

    Build.

--- server/index.js
+++ server/index.js
@@ -12,18 +12,38 @@ const app = express();
 const port = 3001;
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:4000",
+  "https://chat-app-zpvi.onrender.com",
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 204,
+  credentials: true, // Allow credentials like cookies
+};
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 app.use(express.json());
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/chat", chatRouter); // Use chatRouter for /chat route
-
+app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"), (err) => {
+    if (err) {
+      console.error("Error sending file:", err);
+    }
+  });
+});
 app.use(notFound);
 app.use(errorHandler);
 
